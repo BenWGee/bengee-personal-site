27f4580e0f114d1f865e1911b2aed4355546f249
benwgee <deck@steamdeck.home>
2024-10-20 23:06:48 +0100

    vault backup: 2024-10-20 23:06:48

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🚀  Dungeons & Debris",
+    pageTitle: "bengee.xyz",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
